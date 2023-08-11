4abe2c44f6f8e063d60897b0960f9ad7d5aa2059
Alex Strelin <alexstrelin@yandex.ru>
2023-08-11 11:29:16 +0300

    подключиться к серверу mongodb

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
-const { PORT = 3000 } = process.env;
+const {
+  PORT = 3000,
+  MONGODB_URL = 'mongodb://localhost:27017/mestodb',
+} = process.env;
+
+mongoose.connect(MONGODB_URL, {
+  useNewUrlParser: true,
+})
+  .then(() => { console.log('Connected to db'); })
+  .catch((err) => console.log(err));
 
 const app = express();
 
