11ddbb3ed57385b0df29a4885f70967de40bf659
Alex Strelin <alexstrelin@yandex.ru>
2023-08-22 22:42:12 +0300

    сделать поля name, about и avatar необязательными

--- models/user.js
+++ models/user.js
@@ -4,23 +4,23 @@ const validator = require('validator');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'Поле \'name\' должно быть заполнено'],
     minlength: [2, 'Минимальная длина поля \'name\' - 2'],
     maxlength: [30, 'Максимальная длина поля \'name\' - 30'],
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    required: [true, 'Поле \'about\' должно быть заполнено'],
     minlength: [2, 'Минимальная длина поля \'about\' - 2'],
     maxlength: [30, 'Максимальная длина поля \'about\' - 30'],
+    default: 'Исследователь',
   },
   avatar: {
     type: String,
-    required: [true, 'Поле \'avatar\' должно быть заполнено'],
     validate: {
       validator: (avatar) => validator.isURL(avatar),
       message: 'Некорректный URL',
     },
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
   email: {
     type: String,
