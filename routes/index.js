6e7b2f9eae5ea0af52e3e25a69b6b1e43c87df37
Alex Strelin <alexstrelin@yandex.ru>
2023-08-28 00:58:52 +0300

    защитить API авторизацией

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,13 @@ const router = require('express').Router();
 const authRouter = require('./auth');
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
+const auth = require('../middlewares/auth');
 
 const statusCodes = require('../utils/constants').HTTP_STATUS;
 
 router.use('/', authRouter);
+
+router.use(auth);
 
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
