298f229081385ed15d75a994b56310dbbe5741ac
Alex Strelin <alexstrelin@yandex.ru>
2023-08-28 00:20:15 +0300

    создать роут для логина и регистрации

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,19 @@
 const router = require('express').Router();
+const authRouter = require('./auth');
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
+
+const statusCodes = require('../utils/constants').HTTP_STATUS;
+
+router.use('/', authRouter);
 
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
 
 router.use((req, res) => {
-  res.status(404).json({ message: 'Ресурс не найден. Проверьте URL и метод запроса' });
+  res
+    .status(statusCodes.NOT_FOUND)
+    .json({ message: 'Ресурс не найден. Проверьте URL и метод запроса' });
 });
 
 module.exports = router;
